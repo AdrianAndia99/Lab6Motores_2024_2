d9acddcaadb48560c186f77aedeaf55eb8a62e37
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-09-27 13:01:20 -0500

    modelados de a pocos

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class PlayerControler : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    public float speed = 5f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+}
