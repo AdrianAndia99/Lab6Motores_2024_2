db3bdaf691c30297a6d615746359fafb4226ec68
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-09-28 13:50:00 -0500

    Falta que funcione

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -6,6 +6,8 @@ public class PlayerControler : MonoBehaviour
     private Rigidbody rb;
     private Vector2 moveInput;
     public float speed = 5f;
+    private float velocidadMovimiento = 3.0f;
+    private float velocidadRotacion;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,10 +18,20 @@ public class PlayerControler : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
-        rb.MovePosition(transform.position + movement);
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref velocidadRotacion, 0.1f);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            rb.MovePosition(transform.position + moveDirection * velocidadMovimiento * Time.deltaTime);
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
